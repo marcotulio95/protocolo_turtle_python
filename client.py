839b7f71b2e53035b6d29305d82ec3532e9ed8e0
marcotulio95 <marco.tulio95@hotmail.com>
2017-02-15 15:15:02 -0200

    Client enviando uma requisicao simples em JSON

--- client.py
+++ client.py
@@ -1,21 +1,26 @@
 import base64
 import socket
+import json
+from io import StringIO
 
-SERVIDOR = 'localhost'
-PORTA = 8888
-
-dado = b'GET'
-
+request = {
+        "nomeOrigem": "MarcoTulio",
+        "SERVIDOR": "localhost",
+        "PORTA": 8888,
+        "dado": ["arq1.txt", "arq2.txt"]
+    }
 
 print('Iniciando cliente')
+requestJson = json.dumps(request)
 
 socketCliente = 	socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socketCliente.connect((SERVIDOR, PORTA))
+socketCliente.connect((request['SERVIDOR'], request['PORTA']))
 
-socketCliente.sendall(dado) 
+socketCliente.sendall(requestJson.encode())
 
 while True:
 	dado = socketCliente.recv(1024)
 	if not dado: break
 	print("Resposta do servidor: ", dado)
+socketCliente.close()
