8ab35fc924601f78a3bad66a0769de7aa81a4114
marcotulio95 <marco.tulio95@hotmail.com>
2017-02-22 09:59:13 -0300

    Tentando tranformar objeto qualquer em String Json

--- client.py
+++ client.py
@@ -1,26 +1,42 @@
 import base64
 import socket
 import json
+import jsonpickle
 from io import StringIO
 
-request = {
-        "nomeOrigem": "MarcoTulio",
-        "SERVIDOR": "localhost",
-        "PORTA": 8888,
-        "dado": ["arq1.txt", "arq2.txt"]
-    }
+
+##-----------------Definindo o objeto Request -------------------##
+
+class Request (object):
+	
+	def __init__(self, nome, servidor, porta, funcao, valor):
+		self.nome = nome
+		self.servidor = servidor
+		self.porta = porta
+		self.funcao = funcao
+		self.valor = valor
+
 
 print('Iniciando cliente')
-requestJson = json.dumps(request)
 
+r1 = Request("Marco Tulio", "localhost", 8880, "move", 10)
+
+r1Json = jsonpickle.encode(r1)
+
+print (r1Json['nome'])
+
+##r1Json = json.dumps(r1)
+
+"""
 socketCliente = 	socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socketCliente.connect((request['SERVIDOR'], request['PORTA']))
+socketCliente.connect((r1.servidor, r1.porta))
 
-socketCliente.sendall(requestJson.encode())
+socketCliente.sendall(r1Json.encode())
 
 while True:
 	dado = socketCliente.recv(1024)
 	if not dado: break
 	print("Resposta do servidor: ", dado)
 socketCliente.close()
+"""
