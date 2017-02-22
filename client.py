133804b51df51147231849d2ae60241eaa8f3488
marcotulio95 <marco.tulio95@hotmail.com>
2017-02-22 11:45:25 -0300

    Request simples, chamando um Hello World no servidor com JSON

--- client.py
+++ client.py
@@ -1,42 +1,28 @@
 import base64
 import socket
 import json
-import jsonpickle
 from io import StringIO
 
-
-##-----------------Definindo o objeto Request -------------------##
-
-class Request (object):
-	
-	def __init__(self, nome, servidor, porta, funcao, valor):
-		self.nome = nome
-		self.servidor = servidor
-		self.porta = porta
-		self.funcao = funcao
-		self.valor = valor
-
+request = {
+        "nomeOrigem": "MarcoTulio",
+        "SERVIDOR": "localhost",
+        "PORTA": 8888,
+        "funcao": "hel",
+        "valor": 10
+    }
 
 print('Iniciando cliente')
 
-r1 = Request("Marco Tulio", "localhost", 8880, "move", 10)
+requestJson = json.dumps(request)
 
-r1Json = jsonpickle.encode(r1)
-
-print (r1Json['nome'])
-
-##r1Json = json.dumps(r1)
-
-"""
 socketCliente = 	socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socketCliente.connect((r1.servidor, r1.porta))
+socketCliente.connect((request['SERVIDOR'], request['PORTA']))
 
-socketCliente.sendall(r1Json.encode())
+socketCliente.sendall(requestJson.encode())
 
 while True:
 	dado = socketCliente.recv(1024)
 	if not dado: break
 	print("Resposta do servidor: ", dado)
 socketCliente.close()
-"""
--- server.py
+++ server.py
@@ -1,5 +1,34 @@
 import socket
 from io import StringIO
+import json
+from io import StringIO
+
+##-----------------------Definindo o Objeto Turtle--------------------------------##
+class Tartaruga(object):
+	
+	def hello(self):
+		print ("hello world !")
+	
+def error404():
+		print("Error 404")
+
+##-----------------------Funcão Processa Request ---------------------------------##
+
+def processa(dado):
+	
+	t1 = Tartaruga()
+	dict = {"hello" : t1.hello}
+
+	JsonDado =  json.loads(dado.decode())
+	funcao = JsonDado['funcao']
+
+	def switch( x ):
+		try:
+			dict[x]()
+		except:
+			error404()
+
+	switch(funcao)
 
 ##-----------------------Função Main ---------------------------------##
 
@@ -13,11 +42,11 @@ socketServidor.listen(1)
 
 print("...esperando conexoes ...")
 conexao, endereco = socketServidor.accept()
-resposta = '200'
+resposta = '200\n'
 
 while True:
 	dado = conexao.recv(1024)
 	if not dado: break
-	print (dado)
+	processa(dado)
 	conexao.sendall(resposta.encode())
 socketServidor.close()
