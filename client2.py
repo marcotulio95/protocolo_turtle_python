78acdd6ecb98d81b66bfaef862ff54534633508d
marcotulio95 <marco.tulio95@hotmail.com>
2017-03-14 11:15:50 -0300

    Client: 2 desenha uma estrela vermelha

---
+++ client2.py
@@ -0,0 +1,39 @@
+import base64
+import socket
+import json
+from io import StringIO
+
+def enviar(socket, request, SERVIDOR="localhost", PORTA= 8880):
+	requestJson = json.dumps(request)
+	socket.sendto( requestJson.encode() , (SERVIDOR, PORTA))
+
+
+print('Iniciando cliente')
+
+
+##----------------Instaciando nosso Socket----------------------------
+
+socketCliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socketCliente.settimeout(3)
+
+##-----------Definindo uma funcao --------------------------
+
+
+request = {"funcao": "color", "valor": "green"}
+enviar(socketCliente, request)
+
+
+for x in range(360):
+	request = {"funcao": "forward", "valor": 200}
+	enviar(socketCliente, request)
+	
+	request = {"funcao": "left", "valor": 170}
+	enviar(socketCliente, request)
+
+
+while True:
+	dado, endereco = socketCliente.recvfrom(1024)
+	if not dado: break
+	print("Resposta do servidor: ", dado.decode())
+
+socketCliente.close()
