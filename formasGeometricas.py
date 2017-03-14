c4056322696b93b9b6c6a8ab8f7a9eba61f34342
marcotulio95 <marco.tulio95@hotmail.com>
2017-03-14 12:06:11 -0300

    Adicionando o arquivo formasGeometrias, que faz requisicoes ao server de maneira a desenhar 3 formas geometricas diferentes

---
+++ formasGeometricas.py
@@ -0,0 +1,54 @@
+import base64
+import socket
+import json
+from io import StringIO
+
+def enviar(socket, request, SERVIDOR="localhost", PORTA= 8880):
+	requestJson = json.dumps(request)
+	socket.sendto( requestJson.encode() , (SERVIDOR, PORTA))
+
+
+print('Iniciando cliente')
+
+
+##----------------Instaciando nosso Socket----------------------------
+
+socketCliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socketCliente.settimeout(3)
+
+##-----------Definindo uma funcao --------------------------
+def DesenharQuadrado(): ## Desenha um quadrado
+	for x in range(4):
+		request = {"funcao": "forward", "valor": 100}
+		enviar(socketCliente, request)
+		request = {"funcao": "left", "valor": 90}
+		enviar(socketCliente, request)
+
+def DesenharTriangulo(): ## Desenha um triangulo
+	for x in range(4):
+		request = {"funcao": "forward", "valor": 100}
+		enviar(socketCliente, request)
+		request = {"funcao": "left", "valor": 120}
+		enviar(socketCliente, request)
+
+def DesenharCirculo(): ## Desenha circulo atraves da instrução circle
+	request = {"funcao": "circle", "valor": 100}
+	enviar(socketCliente, request)
+
+def DesenharCirculoManualmente(): ## Neste exemplo desenhamos o circulo sem a instrução CIRCLE
+	for x in range(360):
+		request = {"funcao": "forward", "valor": x}
+		enviar(socketCliente, request)
+		request = {"funcao": "right", "valor": x}
+		enviar(socketCliente, request)
+
+
+DesenharCirculoManualmente()
+
+
+while True:
+	dado, endereco = socketCliente.recvfrom(1024)
+	if not dado: break
+	print("Resposta do servidor: ", dado.decode())
+
+socketCliente.close()
